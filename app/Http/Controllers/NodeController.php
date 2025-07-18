7e4516c961cfc3ae05805560e7a10263a159cb70
magangpindad <pindadmagang@gmail.com>
2025-07-18 10:31:07 +0700

    connect

--- app/Http/Controllers/NodeController.php
+++ app/Http/Controllers/NodeController.php
@@ -96,7 +96,7 @@ class NodeController extends Controller
             'name' => 'required|string|max:255',
             'ip' => ['required', 'ip', Rule::unique('nodes', 'ip_address')->ignore($node->id)],
             'endpoint' => 'nullable|string|max:255',
-            'status' => 'required|in:online,offline,partial',
+            'status' => 'nullable|in:online,offline,partial',
             'latitude' => 'required|numeric|between:-90,90',
             'longitude' => 'required|numeric|between:-180,180',
             'description' => 'nullable|string',
@@ -138,7 +138,7 @@ class NodeController extends Controller
     public function updateStatus(Request $request, Node $node)
     {
         $validated = $request->validate([
-            'status' => 'required|in:online,offline,partial',
+            'status' => 'nullable|in:online,offline,partial',
             'response_time' => 'nullable|integer|min:0',
         ]);
 
